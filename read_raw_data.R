f9c8553f37fd8708c340d46faae9849c335c869a
Aaron Gullickson <aarongullickson@gmail.com>
2023-01-30 12:16:42 -0800

    Add better comments for dealing with leading zeroes when reading data

--- read_raw_data.R
+++ read_raw_data.R
@@ -10,17 +10,29 @@ library(readr)
 # IPUMS Data -------------------------------------------------------------------
 
 # read in the IPUMS fixed-width data from gzip file. The resulting data should
-# be called "acs" for American Community Survey
+# be called "acs" for American Community Survey. Because of leading zeroes in 
+# the data, read_fwf will want to interpet some field as character strings when
+# they should all be integers. To force this, be sure to add the following to
+# your read_fwf:
+#    col_types = cols(.default = "i")
+
 
 # drop cases that are missing on met2013 or SEI (i.e. have a zero value)
+
 
 # Tract Data -------------------------------------------------------------------
 
 # read in the social explorer tract data from CSV. Name your object "tracts"
+# note that because of leading zeroes in Geo_STATE and Geo_COUNTY, the readr
+# function will try to read these values in as characters but you want them
+# as integers. To force this, add the following as an argument to your
+# read_csv:
+#   col_types = cols(Geo_STATE = "i", Geo_COUNTY = "i")
 
-# after loading in the tract data, please run these lines of code. It will add
-# the metro area identifier (met2013) based on the State and County ID of the 
-# tract. 
+
+# after loading in the tract data, please run the lines of code below. It will
+# add the metro area identifier (met2013) based on the State and County ID of
+# the tract.
 county_cbsa_crosswalk <- read_csv("input/counties2cbsa.csv")
 tracts <- tracts %>%
   left_join(county_cbsa_crosswalk)
