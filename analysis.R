f9c5c1118748b5bfd7434c9b240040832b35c61b
Aaron Gullickson <aarongullickson@gmail.com>
2023-02-20 12:57:43 -0800

    Remove analysis.R script

--- analysis.R
+++
@@ -1,8 +0,0 @@
-##################################################################################
-# analysis.R
-# Your Name Here
-# This script will use the analytical dataset constructed in organize_data.R
-# to perform the analysis. 
-##################################################################################
-
-load("output/met_area.RData")
